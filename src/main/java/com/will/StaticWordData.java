524b0ebeb1238da9c8d0e1fa980743ce36f35899
zhangchao29 <zhangchao29@xiaomi.com>
2021-10-11 12:27:52 +0800

    flink starter

---
+++ src/main/java/com/will/StaticWordData.java
@@ -0,0 +1,16 @@
+package com.will;
+
+public class StaticWordData {
+
+    /**
+     * Steve Jobs's word
+     */
+    public static String[] STEVE_JOBS_WORD = new String[]{
+            "There’s lots of ways to be as a person and some people express their deep appreciation in different ways.",
+            "But one of the ways that I believe people express their appreciation to the rest of humanity is to make something wonderful and put it out there.",
+            "And you never meet the people, you never shake their hands, you never hear their story or tell yours.",
+            "But somehow in the act of making something with a great deal of care and love, something’s transmitted there.",
+            "And it’s a way of expressing to the rest of our species, our deep appreciation.",
+            "So we need to be true to who we are, and remember what’s really important to us."
+    };
+}
